e976414f932129014909276399c3b38162d5695f
GMaynard1 <george.maynard@noaa.gov>
2025-02-13 10:34:01 -0500

    updated regions to EPUs and added bounding boxes

--- R/fishbot_download.R
+++ R/fishbot_download.R
@@ -19,8 +19,8 @@
 #' 
 #' First pass at this downloads a .csv file within a date range and bounding box
 #'
-#' @param region the region of the dataset to download. Acceptable values include
-#'  'GOM' for Gulf of Maine, 'SNE' for Southern New England, or 'MAB' for Mid-Atlantic
+#' @param EPU the EPU of the dataset to download. Acceptable values include
+#'  'GOM' for Gulf of Maine, 'GB' for Georges Bank, or 'MAB' for Mid-Atlantic
 #'  Bight. The default value is NA (no bounding box applied).
 #' @param startDate The earliest date to return. Must be provided in the format yyyy-mm-dd.
 #'  No default value is provided. 
@@ -43,26 +43,26 @@
 #' @examples
 #'
 #' fishbot_download(
-#'   region = 'GOM',
+#'   EPU = 'GOM',
 #'   startDate = '2023-08-01',
 #'   endDate = '2023-08-31'
 #'   )
 #'   
 #' @export
 
-fishbot_download=function(region=NA,startDate,endDate){
+fishbot_download=function(EPU=NA,startDate,endDate){
   ## Select bounding box, if necessary
-  if(is.na(region)==FALSE){
+  if(is.na(EPU)==FALSE){
     coords=ifelse(
-      region=='GOM',
-      c(1,1,1,1),
+      EPU=='GOM',
+      c(41.70484,44.58965,-71.09674,-67.66667),
       ifelse(
-        region=='SNE',
-        c(1,1,1,1),
+        EPU=='GB',
+        c(39.83,42.33,-70,-65.66),
         ifelse(
-          region=='MAB',
-          c(1,1,1,1),
-          stop("\n Region Not Recognized, please select 'GOM','SNE', or 'MAB' or leave blank \n")
+          EPU=='MAB',
+          c(36,41.87264,-77.22503,-70),
+          stop("\n EPU Not Recognized, please select 'GOM','SNE', or 'MAB' or leave blank \n")
         )
       )
     )
